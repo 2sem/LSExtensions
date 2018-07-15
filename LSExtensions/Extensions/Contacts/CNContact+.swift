8829a836493c9f6c2b18a403a3339a500707332d
leesam <kofsiwon@siwonschool.com>
2018-07-15 23:20:23 +0900

    Solves critical crash

--- LSExtensions/Extensions/Contacts/CNContact+.swift
+++ LSExtensions/Extensions/Contacts/CNContact+.swift
@@ -10,9 +10,21 @@ import UIKit
 import Contacts
 
 extension CNContact {
+    /**
+         Alias for the Full name of this contact
+     */
     public var fullName : String?{
         get{
             return CNContactFormatter.string(from: self, style: .fullName);
+        }
+    }
+    
+    /**
+         Alias for the phonetic(easy to pronounce?) Full name of this contact
+     */
+    public var phoneticFullName : String?{
+        get{
+            return CNContactFormatter.string(from: self, style: .phoneticFullName);
         }
     }
 }
