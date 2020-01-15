671b0c8093dfd7d2282e7480d8e0e2b3dc59e930
leesam <kofsiwon@siwonschool.com>
2020-01-15 22:35:12 +0900

    Adds new method for dispatch queue

--- LSExtensions/Extensions/Thread/DispatchQueue+.swift
+++ LSExtensions/Extensions/Thread/DispatchQueue+.swift
@@ -20,4 +20,52 @@ extension DispatchQueue{
             self.sync(execute: block);
         }
     }
+    
+    /**
+     Submits a work item to a dispatch queue when condition is true and optionally associates it with a
+     dispatch group. The dispatch group may be used to wait for the completion
+     of the work items it references.
+     - parameter group: the dispatch group to associate with the submitted
+     work item. If this is `nil`, the work item is not associated with a group.
+     - parameter flags: flags that control the execution environment of the
+     - parameter qos: the QoS at which the work item should be executed.
+     Defaults to `DispatchQoS.unspecified`.
+     - parameter flags: flags that control the execution environment of the work item.
+     - parameter execute: The work item to be invoked on the queue.
+     - parameter when: Handler to check condition. if it is true, dispatch item will be submitted
+     - parameter queue: Queue for performing condition handler
+     - parameter interval: Interval to wait until next performing condition handler
+     - parameter deadline: Limited time can check condition. If time is over it, dispatch work never be submitted
+     - parameter work: Work Item
+     - SeeAlso: `sync(execute:)`
+     - SeeAlso: `DispatchQoS`
+     - SeeAlso: `DispatchWorkItemFlags`
+     */
+    public func async(when: @escaping () -> Bool, queueForWhen queue: DispatchQueue? = nil, interval: TimeInterval = 0.1, deadline: DispatchTime? = nil, group: DispatchGroup? = nil, qos: DispatchQoS = .unspecified, flags: DispatchWorkItemFlags = [], execute work: @escaping @convention(block) () -> Void){
+        let queue = queue ?? DispatchQueue.init(label: "async.when.queue", qos: qos);
+        queue.async { [weak self] in
+            func check() -> Bool{
+                var value = false;
+                self?.sync {
+                    value = when();
+                }
+                
+                return value;
+            }
+            
+            while(!check()){
+                if let deadline = deadline{
+                    guard DispatchTime.now() < deadline else{
+                        return;
+                    }
+                }
+                
+                Thread.sleep(forTimeInterval: interval);
+            }
+            
+            self?.async(group: group, qos: qos, flags: flags, execute: {
+                work();
+            })
+        }
+    }
 }
