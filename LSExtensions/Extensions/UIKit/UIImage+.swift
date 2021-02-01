5ac94685883d0407b722d06e6309c053da03a749
leesam <kofsiwon@siwonschool.com>
2021-02-01 18:04:31 +0900

    removes duplicated extension file

--- LSExtensions/Extensions/UIKit/UIImage+.swift
+++ LSExtensions/Extensions/UIKit/UIImage+.swift
@@ -10,6 +10,35 @@ import UIKit
 
 extension UIImage{
     /**
+        Returns new image redrawed with given size and background color
+        - parameter size: size of new image
+        - parameter backgroundColor: background color of new image
+        - returns: new image redrawed with given size and background color
+    */
+    public func image(withSize size : CGSize, backgroundColor : UIColor? = nil) -> UIImage{
+        var value = self;
+        
+        UIGraphicsBeginImageContext(size);
+        let ctx = UIGraphicsGetCurrentContext();
+        
+        let rect = CGRect(origin: CGPoint.zero, size: size);
+        //fill with background color if it was specified
+        if let color = backgroundColor{
+            ctx!.setFillColor(color.cgColor);
+            ctx!.fill(rect);
+        }
+        
+        //redraw image with given size
+        self.draw(in: rect);
+        
+        value = UIGraphicsGetImageFromCurrentImageContext()!;
+        
+        UIGraphicsEndImageContext();
+        
+        return value;
+    }
+    
+    /**
          Store PNG Image converted from this image to given url and returns whether success or failed
          - Parameter url: URL where to store PNG file
      */
